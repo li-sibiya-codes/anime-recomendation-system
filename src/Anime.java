cff4f69aec7f7ffc21922290c8b2ec57968f91d0
Lindelihle Sibiya <sibiya.l.codes@gmail.com>
2026-05-12 12:49:51 +0200

    defined an anime object, created enum status and updated the textfile to have a status catagory

--- src/Anime.java
+++ src/Anime.java
@@ -1,3 +1,36 @@
 public class Anime {
-    
+    private final String title;
+    private final String genre;        
+    private Status status;
+    private int episodes;
+
+    public Anime(String title, String genre, Status status, int episodes) {
+        this.title = title;
+        this.genre = genre;
+        this.status = status;
+        this.episodes = episodes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    } 
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
 }
---
+++ src/Status.java
@@ -0,0 +1,3 @@
+public enum Status {
+        ONGOING, COMPLETED, CANCELLED
+    }   
