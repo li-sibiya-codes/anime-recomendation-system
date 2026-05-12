53796be01fd1d7ca4205e81f9372ba6355e3c2bd
Lindelihle Sibiya <sibiya.l.codes@gmail.com>
2026-05-12 16:17:50 +0200

    declaration of classes

---
+++ src/FileHandler.java
@@ -0,0 +1,4 @@
+public class FileHandler {
+    // This class will handle file operations such as reading and writing anime data to a file.
+    // Implementation will be added later.
+}   
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is the entry point of the application.
+        // We will initialize the AnimeManager and FileHandler here in the future.
+    }
+}
